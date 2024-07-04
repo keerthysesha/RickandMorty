03300220bd43a3252309e70087e902672291f21a
Keerthy <keerthyseshasayanam30@gmail.com>
2024-07-04 21:25:02 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import CharacterList from './components/CharacterList';
+
+const App = () => {
+  console.log('Rendering App');
+  return (
+    <div className="App">
+     
+      <CharacterList />
+    </div>
+  );
+};
+
+export default App;
